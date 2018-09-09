3bb3931155014a6027bb19335ae564882d60c435
Jackton Levy <jacktonbrigs@gmail.com>
2018-09-09 14:35:34 -0700

    test commit

--- webapptemplte/main.py
+++ webapptemplte/main.py
@@ -1,1 +1,12 @@
-it = 100+2
+from mongoengine import *
+
+connect('webapp')
+
+
+class User(Document):
+    email = StringField(required=True)
+    first_name = StringField(max_length=50)
+    last_name = StringField(max_length=50)
+
+
+ross = User(email='ross@example.com', first_name='Ross', last_name='Lawley').save()
