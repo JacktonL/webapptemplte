3769c3d62389fc1d0b16b376adb0400d1be51191
Jackton Levy <jacktonbrigs@gmail.com>
2018-09-09 15:38:26 -0700

    test commit

---
+++ webapptemplte/routes/index.py
@@ -0,0 +1,10 @@
+from webapptemplte import app
+from flask import render_template
+
+
+@app.route("/userForm", methods=("GET", "POST"))
+def User():
+
+    return render_template("userForm.html")
+
+
